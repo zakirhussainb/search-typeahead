cfbf4e69c0a1d7de92101bd511e57d24a4621636
zakirhussainb <zakirhussainb2693@gmail.com>
2019-06-09 03:01:14 +0530

    search-typeahead-5: Create a Spring Boot application with all the necessary dependencies(spring cloud, data jpa, security) 	Why: 		To develop a web application 	How: 		By using spring initializr search-typeahead-5

---
+++ src/main/java/com/zakcorp/typeahead/TypeaheadApplication.java
@@ -0,0 +1,13 @@
+package com.zakcorp.typeahead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeaheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeaheadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakcorp/typeahead/TypeaheadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zakcorp.typeahead;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TypeaheadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
