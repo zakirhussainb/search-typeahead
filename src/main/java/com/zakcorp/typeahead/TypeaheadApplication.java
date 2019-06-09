407d977cde72330f7e3fde6c03c6fc66444b3ed7
zakirhussainb <zakirhussainb2693@gmail.com>
2019-06-09 17:55:49 +0530

    search-typeahead-15: Implement the driver logic to insert new words into the trie from the main application class 	Why: 		To insert sample cities in india into the Trie 	How: 		By implementing commandline runner in the SpringBootApplication main class search-typeahead-15

--- src/main/java/com/zakcorp/typeahead/TypeaheadApplication.java
+++ src/main/java/com/zakcorp/typeahead/TypeaheadApplication.java
@@ -1,13 +1,30 @@
 package com.zakcorp.typeahead;
 
+import com.zakcorp.typeahead.service.SearchService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TypeaheadApplication {
+public class TypeaheadApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TypeaheadApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		SearchService service = new SearchService();
+		service.insertNewWord("AJMER");
+		service.insertNewWord("AGRA");
+		service.insertNewWord("BHOPAL");
+		service.insertNewWord("BIDAR");
+		service.insertNewWord("CHENNAI");
+		service.insertNewWord("CUTTAK");
+		service.insertNewWord("DELHI");
+		service.insertNewWord("DHANBAD");
+		service.insertNewWord("GAYA");
+		service.insertNewWord("GURGAON");
+		service.insertNewWord("GUNTUR");
+	}
 }
